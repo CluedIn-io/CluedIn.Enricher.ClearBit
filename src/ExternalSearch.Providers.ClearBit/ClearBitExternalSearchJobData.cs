077597dc98ae84819e549542767bf1eff0ddc034
BenFidge <ben.fidge@btopenworld.com>
2022-01-20 18:48:55 +0700

    Implement provider model (AB#6705) (#3)
    * For AB#6705 - Converted to use Provider Model
    
    * chore: Minor fixes
    
    * Chore: Tidy up
    
    * fix: Language features
    
    * fix: Naming convention fixes

---
+++ src/ExternalSearch.Providers.ClearBit/ClearBitExternalSearchJobData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using CluedIn.Core.Crawling;
+
+namespace CluedIn.ExternalSearch.Providers.ClearBit
+{
+    public class ClearBitExternalSearchJobData : CrawlJobData
+    {
+        public ClearBitExternalSearchJobData(IDictionary<string, object> configuration)
+        {
+           
+        }
+
+        public IDictionary<string, object> ToDictionary()
+        {
+            return new Dictionary<string, object> {
+                
+            };
+        }
+        
+    }
+}
