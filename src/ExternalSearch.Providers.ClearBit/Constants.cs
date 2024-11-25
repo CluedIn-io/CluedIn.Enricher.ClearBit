d3ef5a52cad862776e2bf5087a232904ba861584
Yi-Wei <86753236+ywei390@users.noreply.github.com>
2024-11-25 14:04:46 +0800

    feat: Add guidance note to enricher configuration page (#21)

--- src/ExternalSearch.Providers.ClearBit/Constants.cs
+++ src/ExternalSearch.Providers.ClearBit/Constants.cs
@@ -10,6 +10,31 @@ namespace CluedIn.ExternalSearch.Providers.ClearBit
         public const string ComponentName = "Clearbit";
         public const string ProviderName = "Clearbit";
         public static readonly Guid ProviderId = Core.Constants.ExternalSearchProviders.ClearBitId;
+        public const string Instruction = """
+            [
+              {
+                "type": "bulleted-list",
+                "children": [
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the entity type to specify the golden records you want to enrich. Only golden records belonging to that entity type will be enriched."
+                      }
+                    ]
+                  },
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the vocabulary keys to provide the input for the enricher to search for additional information. For example, if you provide the website vocabulary key for the Web enricher, it will use specific websites to look for information about companies. In some cases, vocabulary keys are not required. If you don't add them, the enricher will use default vocabulary keys."
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+            """;
 
         public struct KeyName
         {
@@ -74,7 +99,10 @@ namespace CluedIn.ExternalSearch.Providers.ClearBit
             //}
         };
 
-        public static Guide Guide { get; set; } = null;
+        public static Guide Guide { get; set; } = new Guide
+        {
+            Instructions = Instruction
+        };
         public static IntegrationType IntegrationType { get; set; } = IntegrationType.Enrichment;
     }
 }
